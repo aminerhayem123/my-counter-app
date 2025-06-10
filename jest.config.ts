1bd3197710e8203a3a59c90e1b17c82e878edf7a
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2025-06-10 16:47:39 +0100

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
+
+export default jestConfig;
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders counter and increments count', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const countElement = screen.getByText(/Count: 0/i);
+  expect(countElement).toBeInTheDocument();
+
+  const incrementButton = screen.getByText('Increment');
+  fireEvent.click(incrementButton);
+  expect(screen.getByText(/Count: 1/i)).toBeInTheDocument();
 });
